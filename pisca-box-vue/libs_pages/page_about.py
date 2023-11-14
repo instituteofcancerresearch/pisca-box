def40af694f7aceb41afd487ad52b3af3e188b93
Heather Grant <29428317+heathergrant@users.noreply.github.com>
2023-11-14 14:51:39 +0000

    Update about section
    added some more text

--- pisca-box-vue/libs_pages/page_about.py
+++ pisca-box-vue/libs_pages/page_about.py
@@ -10,11 +10,19 @@ def add_widgets(include_header):
     image = Image.open('app/static/icr.png')
     st.markdown("""
                 
-                The containerisation and adaption of pisca-beast is a collaboration between the Institute of Cancer Research and Arizona State University.
-                
-                [BEAST](http://beast.community/) was created .... some history  
-                [PISCA](https://github.com/adamallo/PISCA) was created by Diego Mallo .... some history  
-                [pisca-box](https://github.com/rachelicr/pisca-box) was created by Heather Grant and Rachel Alcraft .... some history  
+               [PISCA](https://github.com/adamallo/PISCA)(Phylogenetic Inference using Somatic Chromosomal Alterations) created by Diego Mallo and originally introduced in Martinez et al., 2018, is a Bayesian phylogenetics tool for the modelling of tumour evolution using mutli-region somatic chromosomal alterations (SCA) data. 
+               SCA data can take the form of allele specific copy number from deep genome or SNP arrays (i.e. which chromosomes paternal /maternal have been amplified) or absolute copy number (i.e. total copy number, agnostic to which chromosomes). 
+               
+               [BEAST](http://beast.community/) is a large Bayesian phylogenetic project, which is used for species and pathogen evolution. It produces rooted, time-calibrated trees with a rich array of models including clocks (strict, relaxed) and demographics (constant or exponentially growing populations). 
+               The most common datatype is DNA sequences, although it can also has codon and binary models. There are two BEAST projects, BEAST1 and BEAST2 with an overlapping core framework but some differences in specific models available. 
+               
+               PISCA extends the classic BEAST (1.8.4) framework for the use in somatic evolution. Unlike in species evolution, the ancestor is known to be a diploid, healthy cell.
+               An important modification therefore includes the last common ancestor with an unaltered genomic state (LUCA) as a branch from the 'root' of the tree to the most recent common ancestor (MRCA) of the samples.
+               Another important modification is the use of SCA substitution models with gains, losses (and LOH for allele-specific).
+               
+               [pisca-box](https://github.com/rachelicr/pisca-box) was created by Rachel Alcraft and Heather Grant with support from Diego Mallo. This containerized version bypasses the more involved installation steps and dependency clashes. 
+               Like BEAST, PISCA also requires the creation of XML files to start analysis, which can be done in PISCA-box and tested out to check for errors. 
+               
                 
                 """)
     st.divider()
