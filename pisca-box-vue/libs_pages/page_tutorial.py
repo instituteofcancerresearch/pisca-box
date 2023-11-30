10a5bdb57394fede00f31aed731b0121f8054b01
Rachel Alcraft <132372271+rachelicr@users.noreply.github.com>
2023-11-30 11:26:42 +0000

    Update help doc

--- pisca-box-vue/libs_pages/page_tutorial.py
+++ pisca-box-vue/libs_pages/page_tutorial.py
@@ -5,4 +5,4 @@ def add_widgets(include_header):
     if include_header:
         widgets.page_header('pisca tutorial')
             
-    widgets.show_pdf('app/static/pisca-help-inputs.pdf',height=800)
+    widgets.show_pdf('app/static/pisca-help.pdf',height=800)
