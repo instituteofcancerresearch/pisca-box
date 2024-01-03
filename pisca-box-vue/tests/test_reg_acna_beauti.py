546b5dc9deccfb52d5cbc9c2410463135c7de8d7
Rachel Alcraft <132372271+rachelicr@users.noreply.github.com>
2024-01-03 13:53:42 +0000

    rename tests

---
+++ pisca-box-vue/tests/test_reg_acna_beauti.py
@@ -0,0 +1,124 @@
+# ruff: noqa: F841
+import __init__ # noqa: F401
+import libs.cls_xml as xml
+import libs.cls_mcmc as mc
+import libs.cls_operators as ops
+import pandas as pd
+import libs.cls_datadetermine as dd
+import libs.cls_dt_biallelic as bb
+import libs.cls_dt_acna as ac
+import libs.cls_dt_cnv as cv
+import libs.cls_dt_phyfum as phy
+import libs.cls_priors as prs
+
+this_dir = "/".join(__file__.split('/')[:-1])
+
+
+def help_show_dif(stringa, stringb):
+    stringsa = stringa.split("/>")
+    stringsb = stringb.split("/>")
+    min_len = min(len(stringsa),len(stringsb))
+    for i in range(min_len):
+        stringa = stringsa[i].strip()
+        stringb = stringsb[i].strip()
+        if stringa != stringb:
+            print("LINE=",i,stringa,stringb)
+            print("-----")
+            print(stringa)
+            print("-----")
+            print(stringb)
+            print("-----")
+                                        
+def test_0101(show_xml=False,save_xml=False,overwrite=False,check_assert = True):
+    success = True
+         
+    trials = []
+    trials.append(['set1','acna/acna.fasta','acna/acna_dates.csv','acna',False])
+    trials.append(['set2','acna/acna.csv','acna/acna_dates.csv','acna',False])
+    
+                
+    for key,seq_file,seq_ages,seq_type,as_csv in trials:
+
+        # File selection
+        fasta_string = ""
+        seq_csv = pd.DataFrame()
+        file_name = f'{this_dir}/fixtures/{seq_file}'                
+        nm,tp = file_name.lower().split(".")
+        dtd = dd.DataDetermine(None,tp,file_name)
+        dic_seq,dtyp = dtd.get_seq_data()
+        
+        # ages selection - we want the column with age as the header        
+        csv_ages = pd.read_csv(f'{this_dir}/fixtures/{seq_ages}')
+        
+        if dtyp == 'biallelic':
+            dt_obj =  bb.Biallelic(dic_seq,csv_ages)
+        elif dtyp == 'acna':
+            dt_obj =  ac.Acna(dic_seq,csv_ages)
+        elif dtyp == "cnv":
+            dt_obj =  cv.Cnv(dic_seq,csv_ages)
+        elif dtyp == "phyfum":
+            dt_obj =  phy.Phyfum(dic_seq,csv_ages)
+        else:
+            print("Unrecognised datatype",dtyp)
+                                                                                                    
+        # TREES
+        demographic =  "constant size" #["constant size", "exponential growth"]
+        
+        # MCMC
+        mcmcs = {}
+        mcmcs['name'] = "my name any"
+        mcmcs['chain_length'] = 2500
+        mcmcs['log_every'] = 250
+        mcmcs['mle'] = True
+        
+        # clocks
+        clocks = {}        
+        clocks['type'] = "strict clock"
+        clocks['rate'] = 1.0
+                
+        max_age = csv_ages['age'].max()   
+        min_age = csv_ages['age'].min()
+        lucas = {}
+        lucas["model"] = "Neoplastic progression"
+        lucas["hvalue"] = max_age
+        lucas["hupper"] = 0
+        lucas["hlower"] = min_age
+        lucas["bvalue"] = 1
+        lucas["bupper"] = min_age
+        lucas["blower"] = 0     
+                
+                                                        
+        operators = ops.Operators(demographic,lucas["model"],clocks,dt_obj.ops)
+        prrs = prs.Priors(demographic,dt_obj.prs)
+        mcmc = mc.MCMC(mcmcs,clocks,prrs,dt_obj,operators,[])
+        xmlwriter = xml.XmlWriter(dt_obj,mcmc,lucas,clocks,demographic,dt_obj,operators)
+
+        xmlstr = xmlwriter.get_xml()
+        if show_xml:
+            print(xmlstr)
+        if save_xml:
+            filename = f'{this_dir}/fixtures/{seq_file}_fix.xml'
+            if not overwrite:
+                filename = f'{this_dir}/fixtures/{seq_file}_tst.xml'                
+            with open(filename, "w") as f:
+                f.write(xmlstr)
+                print("saved to",filename)
+        xmlstr = xmlstr.replace("\n","").replace(" ","").replace("\t","")
+        if check_assert:
+            fix_file = f'{this_dir}/fixtures/{seq_file}_fix.xml'
+            fix_str = ""
+            with open(fix_file, "r") as f:
+                fix_str = f.read()
+            fix_str = fix_str.replace("\n","").replace(" ","").replace("\t","")
+            
+            if fix_str != xmlstr:
+                help_show_dif(fix_str,xmlstr)
+                success = False
+            assert fix_str == xmlstr, f"BEAUTI-01 xml strings do not match for {seq_file}"
+        
+            
+if __name__ == "__main__":
+    #test_0101(show_xml=False,save_xml=True,overwrite=True,check_assert = True)
+    test_0101(show_xml=False,save_xml=False,overwrite=False,check_assert = True)
+            
+           
---
+++ pisca-box-vue/tests/test_reg_beauti_biallelic.py
@@ -0,0 +1,143 @@
+# ruff: noqa: F841
+import __init__ # noqa: F401
+import libs.cls_xml as xml
+import libs.cls_dt_biallelic as bb
+import libs.cls_operators as ops
+import libs.cls_mcmc as mc
+import pandas as pd
+import libs.cls_priors as prs
+import libs.cls_datadetermine as dd
+
+this_dir = "/".join(__file__.split('/')[:-1])
+
+
+def help_show_dif(stringa, stringb):
+    min_len = min(len(stringa),len(stringb))
+    for i in range(min_len):
+        if stringa[i] != stringb[i]:
+            print(i,stringa[i],stringb[i])
+            if i > 5 and i < min_len - 5:
+                print(i,stringa[i-3:i+3],stringb[i-3:i+3])
+                
+
+def match_assert(trial_matches,str_store):
+    success = True
+    for keya,keyb in trial_matches:
+        stra = str_store[keya]
+        strb = str_store[keyb]
+        #help_show_dif(stra,strb)
+        assert stra == strb, f"BEAUTI-02 xml strings do not match for {keya} and {keyb}"
+        if stra != strb:
+            success = False
+    return success
+                            
+def test_biallelic_xml(show_xml=False,save_xml=False,overwrite=False,check_assert = True):
+    success = True
+         
+    trials = []        
+    trials.append(['bb-fasta','biallelic/patient1.fasta','biallelic/patient1_ages.csv','bb',False])
+    trials.append(['bb-csv','biallelic/patient1_seq.csv','biallelic/patient1_ages.csv','bb',True])
+    
+    str_store = {}
+    trial_matches = []
+    trial_matches.append(['bb-fasta','bb-csv'])
+    
+    for key,seq_file,seq_ages,seq_type,as_csv in trials:
+                              
+        file_name = f'{this_dir}/fixtures/{seq_file}'                
+        nm,tp = file_name.lower().split(".")
+        dtd = dd.DataDetermine(None,tp,file_name)
+        dic_seq,dtyp = dtd.get_seq_data()
+        
+        # ages selection - we want the column with age as the header        
+        csv_ages = pd.read_csv(f'{this_dir}/fixtures/{seq_ages}')
+        
+                                        
+        # PISCA
+        datatype = seq_type #(cnv / acna / bb)
+                
+        # CLOCK        
+        clock = "strict clock" # ["strict clock", "random local clock"]
+        clock_rate = 0.13
+        clocks = {}        
+        clocks['type'] = clock
+        clocks['rate'] = clock_rate
+        
+        
+        # LUCA
+        #we want to draw the age
+        # 
+        # 
+        # s from column age and the seq id from column taxon
+        max_age = csv_ages['age'].max()   
+        min_age = csv_ages['age'].min()
+        lucas = {}
+        lucas["model"] = "Neoplastic progression"
+        lucas["hvalue"] = max_age
+        lucas["hupper"] = 0
+        lucas["hlower"] = min_age
+        lucas["bvalue"] = 1
+        lucas["bupper"] = min_age
+        lucas["blower"] = 0     
+                
+        # TREES
+        demographic =  "constant size" #["constant size", "exponential growth"]
+        
+        # MCMC
+        mcmcs = {}
+        mcmcs['name'] = "my name any"
+        mcmcs['chain_length'] = 2500
+        mcmcs['log_every'] = 250
+        mcmcs['mle'] = True
+                
+        # PRIORS
+        priors = {}
+        priors['luca_branch'] = {'prior_type':'uniform','value':max_age}
+        priors['clock.rate'] = {'prior_type':'normal','mean':0.0,'std':0.13}
+        priors['biallelicBinary.demethylation'] = {'prior_type':'logNormal','mean':1.0,'std':0.6,'offset':0.0,'realSpace':True}
+        priors['biallelicBinary.homozygousMethylation'] = {'prior_type':'logNormal','mean':1.0,'std':0.6,'offset':0.0,'realSpace':True}
+        priors['biallelicBinary.homozygousDemethylation']= {'prior_type':'logNormal','mean':1.0,'std':0.6,'offset':0.0,'realSpace':True} 
+        
+        # Load DATATYPE
+        dt_obj =  bb.Biallelic(dic_seq,csv_ages)
+        operators = ops.Operators(demographic,lucas["model"],clocks,dt_obj.ops)
+        prrs = prs.Priors(demographic,dt_obj.prs)
+        mcmc = mc.MCMC(mcmcs,clocks,prrs,dt_obj,operators,[])
+        xmlwriter = xml.XmlWriter(dt_obj,mcmc,lucas,clocks,demographic,dt_obj,operators)
+                                                
+        xmlstr = xmlwriter.get_xml()
+        if show_xml:
+            print(xmlstr)
+        if save_xml:
+            filename = f'{this_dir}/fixtures/{seq_file}_fix.xml'
+            if not overwrite:
+                filename = f'{this_dir}/fixtures/{seq_file}_tst.xml'                
+            with open(filename, "w") as f:
+                f.write(xmlstr)
+                print("saved to",filename)
+        xmlstr = xmlstr.replace("\n","").replace(" ","").replace("\t","")
+        if check_assert:
+            fix_file = f'{this_dir}/fixtures/{seq_file}_fix.xml'
+            fix_str = ""
+            with open(fix_file, "r") as f:
+                fix_str = f.read()
+            fix_str = fix_str.replace("\n","").replace(" ","").replace("\t","")
+            
+            if fix_str != xmlstr:
+                help_show_dif(fix_str,xmlstr)
+                success = False
+            assert fix_str == xmlstr, f"BEAUTI-01 xml strings do not match for {seq_file}"
+        
+        str_store[key] = xmlstr
+        
+    succ = match_assert(trial_matches,str_store)
+                
+           
+############################################    
+
+if __name__ == "__main__":
+    #test_biallelic_xml(show_xml=False,save_xml=True,overwrite=True,check_assert = True)
+    test_biallelic_xml(show_xml=False,save_xml=False,overwrite=False,check_assert = True)
+
+    
+    
