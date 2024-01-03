375cd7b4ea09bbb68db75f5fb47aea43cde3ed8f
Rachel Alcraft <132372271+rachelicr@users.noreply.github.com>
2024-01-03 13:54:17 +0000

    fix missing sections

--- pisca-box-vue/libs/cls_datatype.py
+++ pisca-box-vue/libs/cls_datatype.py
@@ -35,16 +35,16 @@ class DataType:
         self.logs['likelihood'] = 'likelihood'
         self.logs['coalescent'] = 'coalescentLikelihood'
         # for clock is random local clock
-        #self.logs['rateChanges'] = 'statistic'        
+        #self.logs['rateChanges'] = 'statistic'
         #self.logs['coefficientOfVariation'] = 'rateStatisticCenancestor'
         #self.logs['covariance'] = 'rateCovarianceStatistic'
         #self.logs['cenancestorRate'] = 'rateStatisticCenancestor'
     ##############################################################
     # ABSTRACT FUNCTIONS TO IMPLEMENT
-    ##############################################################                    
+    ##############################################################
     def _get_datatype(self):
-        raise NotImplementedError("Should have implemented this")        
-    ##############################################################                    
+        raise NotImplementedError("Should have implemented this")
+    ##############################################################
     def _load_data(self):
         raise NotImplementedError("Should have implemented this")
     ##############################################################
@@ -52,15 +52,15 @@ class DataType:
         raise NotImplementedError("Should have implemented this")
     #############################################################
     def _default_priors(self):
-        raise NotImplementedError("Should have implemented this")    
+        raise NotImplementedError("Should have implemented this")
     #############################################################
     def _define_default_logs(self):
-        raise NotImplementedError("Should have implemented this")            
+        raise NotImplementedError("Should have implemented this")
     #############################################################
     ## XML ##
     def get_datatype_xml(self):
         raise NotImplementedError("Should have implemented this")
-    ##############################################################                    
+    ##############################################################
     def get_site_model(self):
         raise NotImplementedError("Should have implemented this")
     ##############################################################
@@ -75,7 +75,7 @@ class DataType:
     ##############################################################
         
     ##############################################################
-    # IMPLEMENTED HELPER FUNCTIONS    
+    # IMPLEMENTED HELPER FUNCTIONS
     ##############################################################                                                                        
     def selected_logs(self,priors,operators,choices=[]):                             
         if choices == []:
@@ -130,32 +130,37 @@ class DataType:
     def get_fasta_alignment(self):
         return self._make_alignment()        
     ##############################################################                    
-    def _make_a_taxon(self,id,age,compartment):
+    def _make_a_taxon(self,idd,age,compartment):
         taxon = ""
-        taxon += f'\t<taxon id="{id}">\n'
+        taxon += f'\t<taxon id="{idd}">\n'
         taxon += f'\t\t<date value="{age}" direction="forwards" units="years"/>\n'
         if compartment != "?":
             taxon += '\t\t<attr name="compartment">\n'
             taxon += f'\t\t\t{compartment}"\n'
-            taxon += '\t\t</attr>\n'            
+            taxon += '\t\t</attr>\n'
         taxon += '\t</taxon>\n'
         return taxon
     ##############################################################
     def _make_taxa(self):
         taxa = '<taxa id="taxa">\n'
-        cpt = "?"      
-        for tx,age in self.dic_taxon_age.items():            
+        cpt = "?"
+        number_taxa = 0
+        for tx,age in self.dic_taxon_age.items():
             if tx in self.dic_taxon_compartment:
-                cpt = self.dic_taxon_compartment[tx]                    
+                cpt = self.dic_taxon_compartment[tx]
             if tx in self.dic_taxon_seq:
                 taxa += self._make_a_taxon(tx,age,cpt)
+                number_taxa += 1
             # don't use it otherwise as there is no seq data
+        if number_taxa == 0:
+            taxa += '\t<!-- No taxa, is there an input error? The ages need to match the sequences. -->\n'
+            print("No taxa, is there an input error? The ages need to match the sequences.")        
         taxa += '</taxa>\n'
         return taxa
     ##############################################################
-    def _make_a_sequence(self,id,seq):
+    def _make_a_sequence(self,idd,seq):
         seqstr = "\t<sequence>\n"
-        seqstr += f'\t\t<taxon idref="{id}"/>\n'
+        seqstr += f'\t\t<taxon idref="{idd}"/>\n'
         seqstr += f'\t\t{seq}\n'
         seqstr += '\t</sequence>\n'
         return seqstr
@@ -163,18 +168,23 @@ class DataType:
     def _make_alignment(self):        
         algn = '<alignment id="alignment">\n'
         algn += f'\t<dataType idref="{self.datatype}"/>\n'
-        for id,seq in self.dic_taxon_seq.items():
-            if id in self.dic_taxon_age:
-                algn += self._make_a_sequence(id,seq.strip())        
-            # don't use it otherwise as there is no age data
+        number_alignmnets = 0
+        for idd,seq in self.dic_taxon_seq.items():
+            if idd in self.dic_taxon_age:
+                algn += self._make_a_sequence(idd,seq.strip())        
+                number_alignmnets += 1
+            # don't use it otherwise as there is no age data        
+        if number_alignmnets == 0:
+            algn += '\t<!-- No alignnents, is there an input error? The ages need to match the sequences. -->\n'
+            print("No alignnents, is there an input error? The ages need to match the sequences.")        
         algn += '</alignment>\n'
         return algn        
     ##############################################################
-    def _load_ages_data(self):        
+    def _load_ages_data(self):
         ages = self.ages_csv["age"]
         taxa = self.ages_csv["taxon"]
         for i in range(len(ages)):
-            taxon = taxa[i]
+            taxon = taxa[i].strip()
             age = ages[i]
             self.dic_taxon_age[taxon] = age     
         if "compartment" in self.ages_csv.columns:
